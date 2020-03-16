68fa021b77410928770fc1462492faf51a971481
Jasper Cheung <jaspercheung6@gmail.com>
2020-03-16 16:08:38 -0400

    return datetime objects

--- gql/renderer_dataclasses.py
+++ gql/renderer_dataclasses.py
@@ -146,14 +146,14 @@ class DataclassesRenderer:
         is_enum = field.type in enum_names
         suffix = ''
         field_type = field.type
-        print(field.type)
         if is_enum:
             suffix = f'= enum_field({field.type})'
 
         if field.type == 'DateTime':
-            field_type = 'DATETIME_FIELD'
+            field_type = 'datetime'
+            suffix = '=DATETIME_FIELD'
 
-        if field.nullable:
+        elif field.nullable:
             suffix = f'= {field.default_value}'
 
         buffer.write(f'{field.name}: {field_type} {suffix}')
